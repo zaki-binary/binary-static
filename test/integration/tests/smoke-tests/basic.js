333069e957338e6e6dd1fb8dd70040e30ab3ccc2
Boris Yankov <borisyankov@gmail.com>
2014-07-21 23:08:33 +0300

    Add initial smoke tests

---
+++ test/integration/tests/smoke-tests/basic.js
@@ -0,0 +1,45 @@
+﻿var utils = require('../../utils');
+
+var urls = [
+    '',
+    '/why-us',
+    '/get-started',
+    '/tour',
+    '/c/trade.cgi',
+    '/c/contact.cgi',
+
+    '/c/linkto_acopening.cgi?actype=real',
+    '/c/linkto_acopening.cgi?actype=virtual',
+    '/c/affiliate_signup.cgi',
+    '/responsible-trading',
+    '/c/c_template.cgi?filecode=legal',
+
+    '/d/tick_trades.cgi',
+    '/c/pricing_table.cgi',
+    '/c/rise_fall_table.cgi',
+    '/c/asset_index.cgi',
+
+    '/c/available_payment_methods.cgi',
+    '/c/c_template.cgi?filecode=legal',
+
+    '/why-us',
+    '/get-started',
+    '/tour',
+
+    '/c/chart_application.cgi',
+    '/d/smartchart.cgi',
+    '/c/livechart.cgi',
+
+    '/about-us',
+    '/group-history',
+    '/careers',
+    '/partnerapi',
+
+    '/c/contact.cgi',
+    '/c/us_patents.cgi',
+    '/open-source-projects'
+];
+
+module.exports.smokeBasic = function (browser) {
+    utils.openUrls(browser, urls);
+};
--- test/integration/utils.js
+++ test/integration/utils.js
@@ -1,8 +1,23 @@
-module.exports.randomStr = function(length, initChars) {
+module.exports.randomStr = function (length, initChars) {
     var chars = initChars || 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
         str = '';
     for (var i = length; i > 0; --i) {
         str += chars[Math.round(Math.random() * (chars.length - 1))];
     }
     return str;
+};
+
+module.exports.openUrls = function (browser, urls) {
+
+    urls.forEach(function (url) {
+
+        var visitUrl = browser.globals.url + url;
+
+        browser
+            .url(visitUrl, function () {
+                console.log("Opening ", visitUrl);
+            })
+            .waitForElementVisible('body', 5000);
+    });
+    browser.end();
 };
