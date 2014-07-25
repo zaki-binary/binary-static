07db38b72dcaec8d531426fdc03597d944917d73
Boris Yankov <borisyankov@gmail.com>
2014-07-25 11:24:19 +0300

    Add radom trades tests

---
+++ test/integration/tests/trades/checkRandomRates.js
@@ -0,0 +1,15 @@
+﻿var element = {    
+    amountFor10: '#amount_for_10',
+    unitsFor10: '#units_for_10'
+};
+
+module.exports = {
+
+    "checkRandomRates": function (browser) {
+        browser
+            .url(browser.globals.url + '/c/trade.cgi?market=random')
+            .waitForElementVisible(element.amountFor10, 5000)
+            .assert.containsText(element.unitsFor10, '50')
+        .end();
+    },
+};
