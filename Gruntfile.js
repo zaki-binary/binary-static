fefc3b5741842e6bcbe2c5bcbd90600506303aff
Boris Yankov <borisyankov@gmail.com>
2014-08-06 05:04:23 +0300

    Add Java detection for Chart app, display a button accordingly

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,6 +57,7 @@ module.exports = function (grunt) {
 		    'src/javascript/external/jstree/jquery.jstree.js',
 		    'src/javascript/external/jquery-ui-timepicker/jquery.ui.timepicker.js',
 		    'src/javascript/external/eventsource.js',
+		    'src/javascript/external/deployJava.js',
 		    'src/javascript/base/onerror.js',
 		    'src/javascript/base/pjax-lib.js',
 		    'src/javascript/external/mmenu/jquery.mmenu.min.all.js',
