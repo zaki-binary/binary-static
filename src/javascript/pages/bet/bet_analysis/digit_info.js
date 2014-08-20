07cbcfb6ab755275cd426247a5e2e68fa42fbeb1
Jun Bon <junbon@regentmarkets.com>
2014-08-20 05:45:14 +0000

    enabled x-axis label

--- src/javascript/pages/bet/bet_analysis/digit_info.js
+++ src/javascript/pages/bet/bet_analysis/digit_info.js
@@ -39,7 +39,6 @@ BetAnalysis.DigitInfo = function() {
         },
         xAxis:{
             categories: ['0','1','2','3','4','5','6','7','8','9'],
-            labels:{ enabled:false},
             lineWidth:0,
             lineColor:'#999',
             tickLength:10,
