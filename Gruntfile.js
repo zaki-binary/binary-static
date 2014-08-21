8bf4fee3cabde7ce4182cde2fd5422f4fbd5365b
Raunak Kathuria <raunakkathuria@gmail.com>
2014-08-21 16:45:49 +0800

    Add config.json to be committed with bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -148,7 +148,7 @@ module.exports = function (grunt) {
                 updateConfigs: [],
                 commit: true,
                 commitMessage: 'Release v%VERSION%',
-                commitFiles: ['package.json'],
+                commitFiles: ['package.json', 'config.json'],
                 createTag: false,
                 push: false,
            },
