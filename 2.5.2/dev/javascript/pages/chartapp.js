42aac20f174412e15862c83ee162ccf81841e423
Raunak Kathuria <raunakkathuria@gmail.com>
2014-08-25 09:59:36 +0800

    Auto-generated commit

---
+++ 2.5.2/dev/javascript/pages/chartapp.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    var isMac = /Mac/i.test(navigator.platform),
+        isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent),
+        isAndroid = /Android/i.test(navigator.userAgent),
+        isWindowsPhone = /Windows Phone/i.test(navigator.userAgent),
+        isJavaInstalled = (deployJava.getJREs().length > 0) && deployJava.versionCheck("1.5+"),
+        isMobile = isIOS || isAndroid || isWindowsPhone,
+        canBeInstalled = isJavaInstalled && !isMobile;
+
+    $('#install-java').toggle(!isJavaInstalled);
+    $('#download-app').toggle(canBeInstalled);
+
+    $('#install-java').on('click', function () {
+        deployJava.installLatestJava();
+    });
+
+    $('#download-app').on('click', function () {
+        if (isMac) {
+            alert('You need to change your security preferences!');
+            return;
+        }
+
+        if (isMobile) {
+            alert('The charting app is not available on mobile devices!');
+        }
+    });
+})();
