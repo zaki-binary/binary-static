50c1f8b4580bb373d2277e505a870835e8ed1192
Boris Yankov <borisyankov@gmail.com>
2014-08-29 07:02:39 +0300

    Fix fileindex task for 'javascript.json'

--- build/fileindex.js
+++ build/fileindex.js
@@ -1,14 +1,34 @@
 module.exports = {
     all: {
-        javascript: {
-            options: {
-                format: 'json_flat',
-                pretty: true
-            },
-            files: [
-                { dest: 'dist/app.json', src: ['src/javascript/**/*.js', '!src/javascript/external/**/*.js'] },
-                { dest: 'dist/lib.json', src: ['src/javascript/external/**/*.js'] }
+        options: {
+            format: 'json_flat',
+            pretty: true
+        },
+        files: [{ 
+            dest: 'dist/javascript.json', 
+            src: [
+            'src/javascript/lib/jquery.js', 
+            'src/javascript/lib/highstock/highstock.js', 
+            'src/javascript/lib/moment/moment.js', 
+            'src/javascript/lib/**/*.js',
+            'src/javascript/binary/base/*.js',
+            'src/javascript/binary/**/*.js'
             ]
-        }
+        },
+        { 
+            dest: 'dist/lib.json', 
+            src: [
+            'src/javascript/lib/jquery.js', 
+            'src/javascript/lib/highstock/highstock.js', 
+            'src/javascript/lib/moment/moment.js', 
+            'src/javascript/lib/**/*.js'
+            ]
+        },
+        { 
+            dest: 'dist/binary.json', src: [
+            'src/javascript/binary/base/*.js',
+            'src/javascript/binary/**/*.js'
+            ] 
+        }]
     }
 };
