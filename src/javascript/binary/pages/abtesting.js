cfce1e5d4f8fcd714718c0cba7faa5c429d23a4a
Ashkan <ashkan.nemati@gmail.com>
2015-02-05 12:11:29 +0800

    upload home3 & home4

---
+++ src/javascript/binary/pages/abtesting.js
@@ -0,0 +1,27 @@
+var signup = function() {
+    var signup = $('.login-button, .dialog, .overlay').on('click', function() {
+        $('.overlay').toggleClass('hidden');
+    })
+};
+var xscroll = function() {
+    var xscroll = $("#scroll").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#section2").offset().top
+        }, 1000);
+    });
+};
+pjax_config_page('/home3', function() {
+    return {
+        onLoad: function() {
+            signup();
+        },
+    };
+});
+pjax_config_page('/home4', function() {
+    return {
+        onLoad: function() {
+            signup();
+            xscroll();
+        },
+    };
+});
