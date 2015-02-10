a25542d1e5c654a6fa7e950be17822f6662040d0
Raunak Kathuria <raunakkathuria@gmail.com>
2015-02-10 11:02:18 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -2981,6 +2981,36 @@ onLoad.queue_for_url(function() {
         return false;
     });
 }, '/c/paymentagent_list');
+;var signup = function() {
+    var signup = $('.login-button, .dialog, .overlay').on('click', function() {
+        $('.overlay').toggleClass('hidden');
+    });
+};
+
+var xscroll = function() {
+    var xscroll = $("#xscroll").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#section2").offset().top
+        }, 1000);
+    });
+};
+
+pjax_config_page('/home3', function() {
+    return {
+        onLoad: function() {
+            signup();
+        },
+    };
+});
+
+pjax_config_page('/home4', function() {
+    return {
+        onLoad: function() {
+            signup();
+            xscroll();
+        },
+    };
+});
 ;var trade_contract_back = function () {
     $('#find_another_contract').on('click', function (e) {
         if (page.url.history_supported) {
