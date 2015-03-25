c6417f0d037ce331a9ed03e9f54cd7260bace421
Raunak Kathuria <raunakkathuria@gmail.com>
2015-03-25 11:16:43 +0800

    Fix state list not loading on open account page refresh

--- src/javascript/binary/pages/client.js
+++ src/javascript/binary/pages/client.js
@@ -25,14 +25,16 @@ onLoad.queue(function() {
         client_form = new ClientForm({restricted_countries: page.settings.get('restricted_countries'), valid_loginids: page.settings.get('valid_loginids')});
 });
 
-$(function() {
-    onLoad.queue_if_id_present(function () {
-        $('#promotionalcode').blur(togglePromoCodeTnC);
-        client_form.on_residence_change();
-        select_user_country();
-        if(page.client.is_logged_in) {
-            client_form.set_virtual_login_id(page.client.loginid);
-            client_form.set_virtual_email_id(page.client.email);
+pjax_config_page('linkto_acopening', function() {
+    return {
+        onLoad: function() {
+            $('#promotionalcode').blur(togglePromoCodeTnC);
+            client_form.on_residence_change();
+            select_user_country();
+            if(page.client.is_logged_in) {
+                client_form.set_virtual_login_id(page.client.loginid);
+                client_form.set_virtual_email_id(page.client.email);
+            }
         }
-    },'openAccForm');
+    };
 });
