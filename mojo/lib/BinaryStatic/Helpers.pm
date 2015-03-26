1629ffe8ba6b9970c13ffad3cc2a24007a61f630
Fayland Lam <fayland@gmail.com>
2015-03-26 17:51:28 +0800

    portfolio.cgi link fix

--- mojo/lib/BinaryStatic/Helpers.pm
+++ mojo/lib/BinaryStatic/Helpers.pm
@@ -250,7 +250,7 @@ sub _main_menu_myaccount {
     push @{$my_account_ref->{sub_items}},
       {
         id         => 'topMenuPortfolio',
-        url        => $self->c->url_for('/portfolio.cgi'),
+        url        => $self->c->url_for('/user/portfolio'),
         text       => $self->c->l('Portfolio'),
         link_class => 'with_login_cookies pjaxload',
       };
