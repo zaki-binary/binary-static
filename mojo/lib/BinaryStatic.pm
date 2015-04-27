3bae075e6864d3912dc70567b3b2b49f02fb83f4
Fayland Lam <fayland@gmail.com>
2015-04-27 14:03:22 +0800

    small naming add

--- mojo/lib/BinaryStatic.pm
+++ mojo/lib/BinaryStatic.pm
@@ -106,10 +106,10 @@ sub startup {
     $r->get('/logout')->to('page#logout');
 
     $r->get('/user/open_account')->to('page#toolkit');
-    $r->get('/affiliate/signup')->to('page#toolkit');
+    $r->get('/affiliate/signup')->to('page#toolkit')->name('affiliate-signup');
     $r->get('/resources/pricing_table')->to('page#toolkit');
-    $r->get('/charting/application')->to('page#toolkit');
-    $r->get('/charting/livechart')->to('page#toolkit');
+    $r->get('/charting/application')->to('page#toolkit')->name('charting-application');
+    $r->get('/charting/livechart')->to('page#toolkit')->name('charting-livechart');
     $r->get('/resources/rise_fall_table')->to('page#toolkit');
 
     $r->route('/exception')->to('page#exception');
