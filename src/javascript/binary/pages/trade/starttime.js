ef537a80bce3ca795ff81235b25e75456b9ccefd
Raunak Kathuria <raunakkathuria@gmail.com>
2015-07-22 10:30:08 +0800

    add js for display/calculate starttime

---
+++ src/javascript/binary/pages/trade/starttime.js
@@ -0,0 +1,51 @@
+function compareStartDate(a,b) {
+  if (a.date < b.date)
+    return -1;
+  if (a.date > b.date)
+    return 1;
+  return 0;
+}
+
+function displayStartDates(id, startDates) {
+
+    if (startDates) {
+        var target= document.getElementById(id),
+            fragment =  document.createDocumentFragment(),
+            option = document.createElement('option'),
+            content = document.createTextNode('Now');
+
+        while (target && target.firstChild) {
+            target.removeChild(target.firstChild);
+        }
+
+        option.setAttribute('value', 'now');
+        option.appendChild(content);
+        fragment.appendChild(option);
+
+        startDates.sort(compareStartDate);
+
+        for (var i = 0, len = startDates.length; i < len; i++) {
+            var a = moment.unix(startDates[i].open).utc();
+            var b = moment.unix(startDates[i].close).utc();
+
+            var ROUNDING = 5 * 60 * 1000;
+            var start = moment();
+            start = moment(Math.ceil((+start) / ROUNDING) * ROUNDING).utc();
+
+            if (moment(a, 'YYYY MM DD').isSame(moment(start ,'YYYY MM DD'))) {
+                a = start;
+            }
+
+            while(a.isBefore(b)) {
+                option = document.createElement('option');
+                content = document.createTextNode(a.format('HH:mm ddd'));
+                option.appendChild(content);
+                fragment.appendChild(option);
+                a.add(5, 'minutes');
+            }
+        }
+        target.appendChild(fragment);
+    } else {
+        document.getElementById('date_start_row').style.display = 'none';
+    }
+}
