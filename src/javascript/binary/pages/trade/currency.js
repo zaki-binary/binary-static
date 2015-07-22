13a993332451d79be137064c4ff44736528c9701
Raunak Kathuria <raunakkathuria@gmail.com>
2015-07-22 17:11:42 +0800

    js file to display currency

---
+++ src/javascript/binary/pages/trade/currency.js
@@ -0,0 +1,24 @@
+var displayCurrencies = function (input, selected) {
+    var target= document.querySelector('#currency'),
+        fragment =  document.createDocumentFragment(),
+        currencies = input.currencies;
+
+    while (target && target.firstChild) {
+        target.removeChild(target.firstChild);
+    }
+
+    for (var i = 0, len = currencies.length; i < len; i++) {
+        var option = document.createElement('option'),
+            content = document.createTextNode(currencies[i]);
+
+        option.setAttribute('value', currencies[i]);
+
+        if (selected && selected == key) {
+            option.setAttribute('selected', 'selected');
+        }
+
+        option.appendChild(content);
+        fragment.appendChild(option);
+    }
+    target.appendChild(fragment);
+};
