2164f3aede1b4321540f6992a7a0930ae01b09c1
Jun Bon Kok <junbon@binary.com>
2015-07-27 17:13:47 +0800

    hide bet_calculation on params change and store them into localstorage

--- src/javascript/binary/pages/bet/bet_form.js
+++ src/javascript/binary/pages/bet/bet_form.js
@@ -224,6 +224,23 @@ var BetForm = function () {
                     $('#stop_type').on('change', function (e) {
                         var selected = $(this).val();
                         $('#stop_type_2').val(selected);
+                        BetForm.attributes.model.stop_type(selected);
+                        $('#bet_calculation_container').hide();
+                    }).addClass('unbind_later');
+                    $('#stop_profit').on('change', function (e) {
+                        var selected = $(this).val();
+                        BetForm.attributes.model.stop_profit(selected);
+                        $('#bet_calculation_container').hide();
+                    }).addClass('unbind_later');
+                    $('#stop_loss').on('change', function (e) {
+                        var selected = $(this).val();
+                        BetForm.attributes.model.stop_loss(selected);
+                        $('#bet_calculation_container').hide();
+                    }).addClass('unbind_later');
+                    $('#amount_per_point').on('change', function (e) {
+                        var selected = $(this).val();
+                        BetForm.attributes.model.amount_per_point(selected);
+                        $('#bet_calculation_container').hide();
                     }).addClass('unbind_later');
                 },
                 on_form_submit: function() {
