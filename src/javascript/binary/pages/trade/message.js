c10e03a172782bab88a7d36001ff794ca19d189d
Raunak Kathuria <raunakkathuria@gmail.com>
2015-08-03 11:55:29 +0800

    add javascript for processing the websocket request type

---
+++ src/javascript/binary/pages/trade/message.js
@@ -0,0 +1,30 @@
+var Message = (function () {
+    'use strict';
+
+    var process = function (msg) {
+        var response = JSON.parse(msg.data);
+        console.log(response);
+        if (response) {
+            var type = response.msg_type;
+
+            if (type == 'offerings') {
+                var market = sessionStorage.getItem('market') || 'Random';
+                processMarketOfferings(response, market);
+            } else if (type == 'contracts') {
+                processContractFormOfferings(response, Trade.form());
+            } else if (type == 'currencies') {
+                displayCurrencies(response);
+                processPriceRequest();
+            } else if (type == 'proposal') {
+                Price.display(response, Contract.contractType()[Trade.form()], document.getElementById('spot'));
+            }
+        } else {
+            console.log('some error occured');
+        }
+    };
+
+    return {
+        process: process,
+    };
+
+})();
