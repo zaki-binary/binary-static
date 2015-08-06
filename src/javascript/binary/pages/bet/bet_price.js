70826cd059ac7f5f14fe1a9ae8b9ccffa24dc804
Jun Bon Kok <junbon@binary.com>
2015-08-06 17:36:49 +0800

    more display changes

--- src/javascript/binary/pages/bet/bet_price.js
+++ src/javascript/binary/pages/bet/bet_price.js
@@ -267,12 +267,15 @@ var BetPrice = function() {
                         that.err_con().find('p').text(resp.error);
                         that.err_con().show();
                     } else {
-                        var color = resp.value.dollar > 0 ? 'profit' : 'loss';
-                        con.find('#status').text(text.localize('Closed')).addClass(color);
-                        con.find('#pnl_value').text(resp.value.dollar).addClass(color);
+                        con.find('#status').addClass('loss').text(text.localize('Closed'));
+                        that.paint_it(resp.value.dollar, con.find('#pnl_value').text(resp.value.dollar));
                         con.find('#pnl_point').text(resp.value.point);
                         con.find('#exit_level').text(resp.exit_level).parents('tr').show();
                     }
+                },
+                paint_it: function(value, target) {
+                    var color = value > 0 ? 'profit' : 'loss';
+                    $(target).removeClass().addClass(color);
                 },
                 stream: function(channel) {
                     var that = this;
@@ -299,7 +302,7 @@ var BetPrice = function() {
                                     con.find('.close_position').show();
                                     con.find('#sell_level').text(level);
                                     var current_value = that.round(parseFloat(prices[i].value.dollar),2);
-                                    con.find('#pnl_value').text(current_value);
+                                    that.paint_it(current_value, con.find('#pnl_value').text(current_value));
                                     con.find('#pnl_point').text(prices[i].value.point);
                                 }
                             }
