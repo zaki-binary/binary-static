05355e068fb13ebce2dff2aed9c595413765ab4a
binary-maksym <maksym@binary.com>
2015-09-02 13:26:15 +0800

    updated ws url

--- src/javascript/binary/pages/trade/socket.js
+++ src/javascript/binary/pages/trade/socket.js
@@ -14,8 +14,7 @@ var TradeSocket = (function () {
     'use strict';
 
     var tradeSocket,
-        // socketUrl = "wss://ws.binary.com/websockets/contracts",
-        socketUrl = "wss://www.binaryqa09.com/websockets/contracts",
+        socketUrl = "wss://ws.binary.com/websockets/v2",
         bufferedSends = [];
 
     var status = function () {
