6eac47a75fbf1ba7a2ac045c99c94911f0e85085
negar <negar@binary.com>
2015-09-14 12:03:39 +0800

    fix limitation

--- src/javascript/binary/pages/trade/price.js
+++ src/javascript/binary/pages/trade/price.js
@@ -137,7 +137,16 @@ var Price = (function () {
             row.appendChild(amount);
         }
 
-        if (details['error'] || !document.getElementById('websocket_form').checkValidity()) {
+        if (!document.getElementById('websocket_form').checkValidity()) {
+            if (purchase) {
+                purchase.style.display = 'none';
+            }
+            if (description) {
+                description.style.display = 'none';
+            }
+        }
+
+        if (details['error']) {
             if (purchase) {
                 purchase.style.display = 'none';
             }
