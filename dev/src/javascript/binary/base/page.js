1d29022da58f2da514036e3721f4095740e7e5ae
Tee Shuwn Yuan <shuwnyuan@binary.com>
2015-10-21 14:16:03 +0800

    Auto-generated commit

--- dev/src/javascript/binary/base/page.js
+++ dev/src/javascript/binary/base/page.js
@@ -243,7 +243,7 @@ Menu.prototype = {
                 this.show_main_menu();
             }
         } else {
-            var is_mojo_page = /^\/$|\/login|\/home|\/smart-indices|\/ad|\/open-source-projects|\/white-labels|\/bulk-trader-facility|\/partners|\/payment-agent$/.test(window.location.pathname);
+            var is_mojo_page = /^\/$|\/login|\/home|\/smart-indices|\/ad|\/open-source-projects|\/white-labels|\/bulk-trader-facility|\/partners|\/payment-agent|\/about-us$/.test(window.location.pathname);
             if(!is_mojo_page) {
                 trading.addClass('active');
                 this.show_main_menu();
