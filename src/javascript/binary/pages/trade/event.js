992b4289a538a40f26d612c119f822d768e85747
binary-maksym <maksym@binary.com>
2015-10-21 17:30:38 +0800

    fix

--- src/javascript/binary/pages/trade/event.js
+++ src/javascript/binary/pages/trade/event.js
@@ -413,7 +413,7 @@ var TradingEvents = (function () {
 
         var view_button = document.getElementById('contract_purchase_button');
         if(view_button){
-            tip.addEventListener('click', debounce( function (e) {
+            view_button.addEventListener('click', debounce( function (e) {
                 BetSell.sell_at_market($(e.traget)[0]);
             }));
         }
