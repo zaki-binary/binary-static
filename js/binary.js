51284f808c9bc785d0e6b66ddee4b582a715db9a
Tee Shuwn Yuan <shuwnyuan@binary.com>
2015-10-27 14:17:05 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -11920,7 +11920,8 @@ var TradingEvents = (function () {
             minDate: new Date(),
             dateFormat: "yy-mm-dd"
         });
-        $(".pickatime" ).timepicker();
+        var date = new Date();
+        $(".pickatime" ).timepicker({minTime:{hour: date.getUTCHours(), minute: date.getUTCMinutes()}});
     };
 
     return {
