bb80fcaa0efc1103bc660db13cb401f0e18b82df
qingwei <qingwei@binary.com>
2015-10-27 18:45:01 +0800

    changed init date to utc

--- src/javascript/binary/static/statement/statement.init.js
+++ src/javascript/binary/static/statement/statement.init.js
@@ -147,7 +147,7 @@ var StatementWS = (function(){
     function initPage(){
 
 
-        StatementUI.setDatePickerDefault(new Date());
+        StatementUI.setDatePickerDefault(moment.utc());
         StatementUI.showButtonOnDateChange();
 
         $("#submit-date").click(function(){
