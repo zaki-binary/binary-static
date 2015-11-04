e9b3ba61f0ba86d73d836034ecbf35ae49fa8632
negar <negar@binary.com>
2015-11-04 14:28:35 +0800

    only make 2 decimal points if number has decimal points

--- src/javascript/binary/pages/trade/event.js
+++ src/javascript/binary/pages/trade/event.js
@@ -158,7 +158,9 @@ var TradingEvents = (function () {
         var amountElement = document.getElementById('amount');
         if (amountElement) {
             amountElement.addEventListener('input', debounce( function(e) {
-                e.target.value = parseFloat(e.target.value).toFixed(2);
+                if (e.target.value % 1 != 0) {
+                    e.target.value = parseFloat(e.target.value).toFixed(2);
+                }
                 sessionStorage.setItem('amount', e.target.value);
                 processPriceRequest();
                 submitForm(document.getElementById('websocket_form'));
@@ -364,7 +366,9 @@ var TradingEvents = (function () {
         var stopLossElement = document.getElementById('stop_loss');
         if (stopLossElement) {
             stopLossElement.addEventListener('input', debounce( function (e) {
-                e.target.value = parseFloat(e.target.value).toFixed(2);
+                if (e.target.value % 1 != 0) {
+                    e.target.value = parseFloat(e.target.value).toFixed(2);
+                }
                 sessionStorage.setItem('stop_loss',e.target.value);
                 processPriceRequest();
                 submitForm(document.getElementById('websocket_form'));
@@ -377,7 +381,9 @@ var TradingEvents = (function () {
         var stopProfitElement = document.getElementById('stop_profit');
         if (stopProfitElement) {
             stopProfitElement.addEventListener('input', debounce( function (e) {
-                e.target.value = parseFloat(e.target.value).toFixed(2);
+                if (e.target.value % 1 != 0) {
+                    e.target.value = parseFloat(e.target.value).toFixed(2);
+                }
                 sessionStorage.setItem('stop_profit',e.target.value);
                 processPriceRequest();
                 submitForm(document.getElementById('websocket_form'));
