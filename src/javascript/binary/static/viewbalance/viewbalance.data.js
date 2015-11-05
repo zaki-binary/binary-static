82d2ce33a5a3f6b9423be93608134bcd58147d1a
qingwei <qingwei@binary.com>
2015-11-05 18:39:06 +0800

    fixed redundant language query

--- src/javascript/binary/static/viewbalance/viewbalance.data.js
+++ src/javascript/binary/static/viewbalance/viewbalance.data.js
@@ -2,7 +2,12 @@
 var ViewBalanceData = (function(){
     var initialized = false;
     var balanceSocket;
+
     var url = "wss://"+window.location.host+"/websockets/v3";
+
+    if (page.language()) {
+        url += '?l=' + page.language();
+    }
 
     function subscribeToBalanceStream(cb){
         if (initialized) {
@@ -10,11 +15,6 @@ var ViewBalanceData = (function(){
         }
 
         initialized = true;
-
-        if (page.language()) {
-            url += '?l=' + page.language();
-        }
-
         balanceSocket = new WebSocket(url);
 
         balanceSocket.onmessage = function(msg){
@@ -22,8 +22,9 @@ var ViewBalanceData = (function(){
 
             if (response.msg_type === "authorize") {
                 balanceSocket.send(JSON.stringify({balance: 1}));
-            } else if (response.msg_type === "balance") {
-                console.log("balance received");
+            }
+
+            if (response.msg_type === "balance") {
                 cb(response.balance);
             }
         };
