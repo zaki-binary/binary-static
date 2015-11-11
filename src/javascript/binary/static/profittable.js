a94484f7468aabe4a843b0444654209823f24e50
binary-maksym <maksym@binary.com>
2015-11-11 15:36:08 +0800

    dont close connection

--- src/javascript/binary/static/profittable.js
+++ src/javascript/binary/static/profittable.js
@@ -3,11 +3,9 @@ pjax_config_page("profit_tablews", function(){
     return {
         onLoad: function() {
             Content.populate();
-            TradeSocket.init();
             ProfitTableWS.init();
         },
         onUnload: function(){
-            TradeSocket.close();
             ProfitTableWS.clean();
         }
     };
--- src/javascript/binary/static/statement.js
+++ src/javascript/binary/static/statement.js
@@ -2,12 +2,10 @@ pjax_config_page("statementws", function(){
     return {
         onLoad: function() {
             Content.populate();
-            TradeSocket.init();
             StatementWS.init();
         },
         onUnload: function(){
             StatementWS.clean();
-            TradeSocket.close();
         }
     };
 });
