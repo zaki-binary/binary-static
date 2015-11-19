b5bc38449ec233d831dc634c3f6c02648bb26b42
Raunak Kathuria <raunakkathuria@gmail.com>
2015-11-19 13:48:37 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -49452,7 +49452,6 @@ Menu.prototype = {
             }
             start_trading.attr("href", trade_url);
 
-            $('#menu-top li:eq(3) a').attr('href', trade_url);
             $('#mobile-menu #topMenuStartBetting a.trading_link').attr('href', trade_url);
         }
 
@@ -49460,12 +49459,6 @@ Menu.prototype = {
             event.preventDefault();
             load_with_pjax(trade_url);
         }).addClass('unbind_later');
-
-        $('#menu-top li:eq(3) a').on('click', function(event) {
-            event.preventDefault();
-            load_with_pjax(trade_url);
-        }).addClass('unbind_later');
-
     }
 };
 
