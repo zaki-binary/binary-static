01ce4a8e37cce4d1d7b09323a1fe58ad180421c8
negar <negar@binary.com>
2015-11-26 15:26:41 +0800

    fix typo

--- src/javascript/binary/static/virtual_acc_opening/virtual_acc_opening.ui.js
+++ src/javascript/binary/static/virtual_acc_opening/virtual_acc_opening.ui.js
@@ -5,7 +5,7 @@ var VirtualAccOpeningUI = (function(){
 
         var labels = document.getElementsByTagName('LABEL');
         for (var i = 0; i < labels.length; i++) {
-            if (labels[i].htmlFor != '') {
+            if (labels[i].htmlFor !== '') {
                 var elem = document.getElementById(labels[i].htmlFor);
                 if (elem)
                     elem.label = labels[i];         
