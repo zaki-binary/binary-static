a90517860cd487813afc98ad0ad19b73401ed87c
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-11-27 18:41:37 +0800

    Few changes after review

--- src/javascript/binary/base/page.js
+++ src/javascript/binary/base/page.js
@@ -477,36 +477,38 @@ Header.prototype = {
         var clock = $('#gmt-clock');
 
         function init(){
-            BinarySocket.send({ "time": 1});
-            query_start_time = (new Date().getTime());  
+            BinarySocket.send({ "time": 1,"passthrough":{"client_time" :  moment.utc().unix()}});
         }
+
         BinarySocket.init({
             onmessage : function(msg){
                 var response = JSON.parse(msg.data);
+
                 if (response && response.msg_type === 'time') {
-                    responseMsg(response);
+
+                    var start_timestamp = response.time;
+                    var pass = response.echo_req.passthrough.client_time;
+
+                    that.time_now = ((start_timestamp * 1000) + (moment.utc().unix() - pass));
+                     
+                    var increase_time_by = function(interval) {
+                        that.time_now += interval;
+                    };
+                    var update_time = function() {
+                         clock.html(moment(that.time_now).utc().format("YYYY-MM-DD HH:mm") + " GMT");
+                    };
+                    update_time();
+
+                    clearInterval(clock_handle);
+
+                    clock_handle = setInterval(function() {
+                        increase_time_by(1000);
+                        update_time();
+                    }, 1000);
                 }
             }
         });
-        function responseMsg(response){
-            var start_timestamp = response.time;
-            
-            that.time_now = ((start_timestamp * 1000)+ ((new Date().getTime()) - query_start_time));
-            var increase_time_by = function(interval) {
-                that.time_now += interval;
-            };
-            var update_time = function() {
-                 clock.html(moment(that.time_now).utc().format("YYYY-MM-DD HH:mm") + " GMT");
-            };
-            update_time();
 
-            clearInterval(clock_handle);
-
-            clock_handle = setInterval(function() {
-                increase_time_by(1000);
-                update_time();
-            }, 1000);
-        }
         that.run = function(){
             setInterval(init, 900000);
         };
