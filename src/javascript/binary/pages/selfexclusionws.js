45b7404d8ea71fa2b48343d60f18ccaee8304ba7
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-12-03 23:15:18 +0800

    setting error validations

--- src/javascript/binary/pages/selfexclusionws.js
+++ src/javascript/binary/pages/selfexclusionws.js
@@ -7,7 +7,6 @@ var SelfExlusionWS = (function(){
 
     var init = function(){
         $form   = $("#selfExclusion");
-
         $form.find("button").on("click", function(e){
             e.preventDefault();
             e.stopPropagation();
@@ -16,32 +15,22 @@ var SelfExlusionWS = (function(){
             }
             BinarySocket.send({"authorize": $.cookie('login'), "passthrough": {"value": "set_self_exclusion"}});
         });
-
         BinarySocket.send({"authorize": $.cookie('login'), "passthrough": {"value": "get_self_exclusion"}});
-
     };
-    function isNormalInteger(str) {
+
+    var isNormalInteger= function(str) {
         return /^\+?\d+$/.test(str);
-    }
-    var resetError = function(){
-        //reset error to empty
+    };
+
+    var validateForm = function(frm){
+        var isValid = true;
         $("p.errorfield").each(function(ind,element){
             $(element).text("");
         });
-    };
-    var resetForm = function(){
+
         $(":text").each(function(ind,element){
-            $(element).val("");
-        });
-    };
-    var validateForm = function(frm){
-        var isValid = true;
-       
-        resetError();
-      
-        //Validate number/integer textboxes
-        $(":text").each(function(ind,element){
-            if(!isNormalInteger($(element).val()) && $(element).val())
+            var ele = $(element).val().replace(/ /g, "");
+            if(!isNormalInteger(ele) && (ele.length > 0))
             {
                 if(!/EXCLUDEUNTIL/.test($(element).attr("id")))
                 {
@@ -50,31 +39,16 @@ var SelfExlusionWS = (function(){
                 }
             }
         });
-
-
         if(validateDate() === false){
             isValid = false;
         }
-
         if(isValid === false){
 
             return false;
         }
-
     };
+
     var isAuthorized =  function(response){
-        /*
-
-        if("error" in response) {
-
-            if("message" in response.error) {
-                console.log(response.error.message);
-            }
-            console.log("issue with authorization");
-
-            return false;
-        }*/
-
         if(response.echo_req.passthrough){
             var option= response.echo_req.passthrough.value ;
 
@@ -87,8 +61,8 @@ var SelfExlusionWS = (function(){
                         break;                   
             }
         }
-   
     };
+
     var validateDate = function(){
         return client_form.self_exclusion.validate_exclusion_date();
     };
@@ -96,142 +70,157 @@ var SelfExlusionWS = (function(){
     var populateForm = function(response){
         var res = response.get_self_exclusion;
 
-        //Reset form to empty.
-        //resetForm();
-
-        //check for error in response
         if("error" in response) {
-            var errorMsg = text.localize("Sorry, there is an issue getting your record.");
-
             if("message" in response.error) {
                 console.log(response.error.message);
             }
-            $("#invalidinputfound").text(errorMsg);
             return false;
+        }else{
+            data.max_balance = $("#MAXCASHBAL").val();
+            data.max_turnover = $("#DAILYTURNOVERLIMIT").val();
+            data.max_losses = $("#DAILYLOSSLIMIT").val();
+            data.max_7day_turnover = $("#7DAYTURNOVERLIMIT").val();
+            data.max_7day_losses = $("#7DAYLOSSLIMIT").val();
+            data.max_30day_turnover = $("#30DAYTURNOVERLIMIT").val();
+            data.max_30day_losses = $("#30DAYLOSSLIMIT").val();
+            data.max_open_bets = $("#MAXOPENPOS").val();
+            data.session_duration_limit =  $("#SESSIONDURATION").val();
+            data.exclude_until = $("#EXCLUDEUNTIL").val();
+
+            if(res){
+                $.map(res,function(value,property){
+
+                    switch(property){
+                        case  "max_balance" :
+                               data.max_balance = parseInt(value);
+                               break;
+                        case  "max_turnover" :
+                               data.max_turnover = parseInt(value);
+                               break;
+                        case  "max_losses"   :
+                               data.max_losses = parseInt(value);
+                               break;
+                        case  "max_7day_turnover" :
+                               data.max_7day_turnover = parseInt(value);
+                               break;
+                        case  "max_7day_losses" :
+                               data.max_7day_losses = parseInt(value);
+                               break;
+                        case   "max_30day_turnover" :
+                                data.max_30day_turnover = parseInt(value);
+                                break;
+                        case   "max_30day_losses" :
+                                data.max_30day_losses = parseInt(value);
+                                break;
+                        case   "max_open_bets" :
+                                data.max_open_bets = parseInt(value);
+                                break; 
+                        case   "session_duration_limit"  :
+                                data.session_duration_limit = parseInt(value);
+                                break;
+                        case   "exclude_until"   :
+                                data.exclude_until = value;
+                                break;       
+
+                    }
+
+                });
+            }
         }
-
-        data.max_balance = $("#MAXCASHBAL").val();
-        data.max_turnover = $("#DAILYTURNOVERLIMIT").val();
-        data.max_losses = $("#DAILYLOSSLIMIT").val();
-        data.max_7day_turnover = $("#7DAYTURNOVERLIMIT").val();
-        data.max_7day_losses = $("#7DAYLOSSLIMIT").val();
-        data.max_30day_turnover = $("#30DAYTURNOVERLIMIT").val();
-        data.max_30day_losses = $("#30DAYLOSSLIMIT").val();
-        data.max_open_bets = $("#MAXOPENPOS").val();
-        data.session_duration_limit =  $("#SESSIONDURATION").val();
-        data.exclude_until = $("#EXCLUDEUNTIL").val();
-
-        if(res){
-            $.map(res,function(value,property){
-
-                switch(property){
-                    case  "max_balance" :
-                           data.max_balance = value;
-                           break;
-                    case  "max_turnover" :
-                           data.max_turnover = value;
-                           break;
-                    case  "max_losses"   :
-                           data.max_losses = value;
-                           break;
-                    case  "max_7day_turnover" :
-                           data.max_7day_turnover = value;
-                           break;
-                    case  "max_7day_losses" :
-                           data.max_7day_losses = value;
-                           break;
-                    case   "max_30day_turnover" :
-                            data.max_30day_turnover = value;
-                            break;
-                    case   "max_30day_losses" :
-                            data.max_30day_losses = value;
-                            break;
-                    case   "max_open_bets" :
-                            data.max_open_bets = value;
-                            break; 
-                    case   "session_duration_limit"  :
-                            data.session_duration_limit = value;
-                            break;
-                    case   "exclude_until"   :
-                            data.exclude_until = value;
-                            break;       
-
-                }
-
-            });
-            
-        }
-         //Bind our data here
         $("#MAXCASHBAL").val(data.max_balance);
-        $("#DAILYTURNOVERLIMIT").val(data.max_turnover),
-        $("#DAILYLOSSLIMIT").val(data.max_losses),
-        $("#7DAYTURNOVERLIMIT").val(data.max_7day_turnover),
-        $("#7DAYLOSSLIMIT").val(data.max_7day_losses),
-        $("#30DAYTURNOVERLIMIT").val(data.max_30day_turnover),
-        $("#30DAYLOSSLIMIT").val(data.max_30day_losses),
-        $("#MAXOPENPOS").val(data.max_open_bets),
-        $("#SESSIONDURATION").val(data.session_duration_limit),
-        $("#EXCLUDEUNTIL").val(data.exclude_until)
-
+        $("#DAILYTURNOVERLIMIT").val(data.max_turnover);
+        $("#DAILYLOSSLIMIT").val(data.max_losses);
+        $("#7DAYTURNOVERLIMIT").val(data.max_7day_turnover);
+        $("#7DAYLOSSLIMIT").val(data.max_7day_losses);
+        $("#30DAYTURNOVERLIMIT").val(data.max_30day_turnover);
+        $("#30DAYLOSSLIMIT").val(data.max_30day_losses);
+        $("#MAXOPENPOS").val(data.max_open_bets);
+        $("#SESSIONDURATION").val(data.session_duration_limit);
+        $("#EXCLUDEUNTIL").val(data.exclude_until);
     };
-    var sendRequest = function(){
 
-        var hasChages  = false;
+    var sendRequest = function(){
+        var hasChanges  = false;
         var newData = {
-            "max_balance"  : $("#MAXCASHBAL").val(),
-            "max_turnover" : $("#DAILYTURNOVERLIMIT").val(),
-            "max_losses" : $("#DAILYLOSSLIMIT").val(),
-            "max_7day_turnover" : $("#7DAYTURNOVERLIMIT").val(),
-            "max_7day_losses" : $("#7DAYLOSSLIMIT").val(),
-            "max_30day_turnover" : $("#30DAYTURNOVERLIMIT").val(),
-            "max_30day_losses" : $("#30DAYLOSSLIMIT").val(),
-            "max_open_bets": $("#MAXOPENPOS").val(),
-            "session_duration_limit" :  $("#SESSIONDURATION").val(),
+            "max_balance"  : parseInt($("#MAXCASHBAL").val()) || "",
+            "max_turnover" : parseInt($("#DAILYTURNOVERLIMIT").val()) || "",
+            "max_losses" : parseInt($("#DAILYLOSSLIMIT").val()) || "" ,
+            "max_7day_turnover" : parseInt($("#7DAYTURNOVERLIMIT").val()) || "",
+            "max_7day_losses" : parseInt($("#7DAYLOSSLIMIT").val()) || "",
+            "max_30day_turnover" : parseInt($("#30DAYTURNOVERLIMIT").val()) || "",
+            "max_30day_losses" : parseInt($("#30DAYLOSSLIMIT").val()) || "",
+            "max_open_bets": parseInt($("#MAXOPENPOS").val()) || "" ,
+            "session_duration_limit" :  parseInt($("#SESSIONDURATION").val()),
             "exclude_until" : $("#EXCLUDEUNTIL").val()
         };
-
-        //Check if value changes
         $.map(newData , function(value, property){
             if(value !== data[property])
-                hasChages = true ;
+                hasChanges = true ;
         }); 
-        if(!hasChages){
+        if(hasChanges === false){
             $("#invalidinputfound").text(text.localize("Please provide at least one self-exclusion setting"));
             return false;
+        }else{
+            BinarySocket.send(
+                {
+                  "set_self_exclusion": 1,
+                  "max_balance": parseInt(newData.max_balance),
+                  "max_turnover": parseInt(newData.max_turnover),
+                  "max_losses": parseInt(newData.max_losses),
+                  "max_7day_turnover": parseInt(newData.max_7day_turnover),
+                  "max_7day_losses": parseInt(newData.max_7day_losses),
+                  "max_30day_turnover": parseInt(newData.max_30day_turnover),
+                  "max_30day_losses": parseInt(newData.max_30day_losses),
+                  "max_open_bets": parseInt(newData.max_open_bets),
+                  "session_duration_limit": parseInt(newData.session_duration_limit),
+                  "exclude_until": newData.exclude_until ? newData.exclude_until : null
+                });
+            return true;
         }
-
-        //Send our request , updating page.
-        BinarySocket.send(
-            {
-              "set_self_exclusion": 1,
-              "max_balance": parseInt(newData.max_balance),
-              "max_turnover": parseInt(newData.max_turnover),
-              "max_losses": parseInt(newData.max_losses),
-              "max_7day_turnover": parseInt(newData.max_7day_turnover),
-              "max_7day_losses": parseInt(newData.max_7day_losses),
-              "max_30day_turnover": parseInt(newData.max_30day_turnover),
-              "max_30day_losses": parseInt(newData.max_30day_losses),
-              "max_open_bets": parseInt(newData.max_open_bets),
-              "session_duration_limit": parseInt(newData.session_duration_limit),
-              "exclude_until": newData.exclude_until
-            });
-
-        return true;
-
     };
+
     var responseMessage = function(response){
         if("error" in response) {
-            var errorMsg = text.localize("Operation failed.");
+            var  error = response.error;
+            switch(error.field){
+                case  "max_balance" :
+                       $("#errorMAXCASHBAL").text(text.localize(error.message));
+                       break;
+                case  "max_turnover" :
+                       $("#errorDAILYTURNOVERLIMIT").text(text.localize(error.message));
+                       break;
+                case  "max_losses"   :
+                       $("#errorDAILYLOSSLIMIT").text(text.localize(error.message));
+                       break;
+                case  "max_7day_turnover" :
+                       $("#error7DAYTURNOVERLIMIT").text(text.localize(error.message));
+                       break;
+                case  "max_7day_losses" :
+                       $("#error7DAYLOSSLIMIT").text(text.localize(error.message));
+                       break;
+                case   "max_30day_turnover" :
+                        $("#error30DAYTURNOVERLIMIT").text(text.localize(error.message));
+                        break;
+                case   "max_30day_losses" :
+                        $("#error30DAYLOSSLIMIT").text(text.localize(error.message));
+                        break;
+                case   "max_open_bets" :
+                        $("#errorMAXOPENPOS").text(text.localize(error.message));
+                        break; 
+                case   "session_duration_limit"  :
+                        $("#errorSESSIONDURATION").text(text.localize(error.message));
+                        break;
+                case   "exclude_until"   :
+                        $("#errorEXCLUDEUNTIL").text(text.localize(error.message));
+                        break;       
 
-            if("message" in response.error) {
-                console.log(response.error.message);
             }
-            $("#invalidinputfound").text(errorMsg);
-
             return false;
+        }else{
+            window.location.href = window.location.href;
         }
-        window.location.href = window.location.href;
     };
+
     var apiResponse = function(response){
         var type = response.msg_type;
     
@@ -244,20 +233,14 @@ var SelfExlusionWS = (function(){
         {
             isAuthorized(response);
         }
-
-    } ;
+    };
 
     return {
         init: init,
         apiResponse: apiResponse,
         populateForm : populateForm
     };
-
-
-
 })();
-
-
 pjax_config_page("user/self_exclusionws", function() {
     return {
         onLoad: function() {
@@ -265,7 +248,6 @@ pjax_config_page("user/self_exclusionws", function() {
                 window.location.href = page.url.url_for('login');
                 return;
             }
-             // Check if it is a real account or not
             if((/VRT/.test($.cookie('loginid')))){
                 window.location.href = ("/");
             }
@@ -279,11 +261,7 @@ pjax_config_page("user/self_exclusionws", function() {
                     }
                 }
             });	
-                
-           // date picker for self exclusion
             Exclusion.self_exclusion_date_picker();
-
-            //init commands
             SelfExlusionWS.init();
         }
     };
