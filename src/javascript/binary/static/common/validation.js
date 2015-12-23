6302ca0073c06f7fda2c80631a1f6c4a5cf910f9
negar <negar@binary.com>
2015-12-23 15:47:44 +0800

    password entropy

--- src/javascript/binary/static/common/validation.js
+++ src/javascript/binary/static/common/validation.js
@@ -1,92 +1,80 @@
 var Validate = (function(){
 
-	//give DOM element of error to display
-	function displayErrorMessage(error){
-        error.setAttribute('style', 'display:block');
+  //give DOM element of error to display
+  function displayErrorMessage(error){
+    error.setAttribute('style', 'display:block');
+  }
+
+  //give DOM element or error to hide
+  function hideErrorMessage(error){
+    error.setAttribute('style', 'display:none');
+    var errorMessage = $('#error-message-password');
+    if (errorMessage){
+      errorMessage.remove();
     }
+  }
 
-    //give DOM element or error to hide
-    function hideErrorMessage(error){
-        error.innerHTML = '';
-    	error.setAttribute('style', 'display:none');
+  function handleError(error, text){
+    var span = document.createElement('span');
+    span.id = 'error-message-password';
+    span.innerHTML = text;
+    error.appendChild(span);
+    displayErrorMessage(error);
+  }
+
+  //check validity of email
+  function validateEmail(mail) {
+    if (/^\w+([\+\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)){
+      return true;
     }
+    return false;
+  }
 
-    //check validity of email
-    function validateEmail(mail) {
-
-        if (/^\w+([\+\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)){  
-        
-            return true;
-        }  
-        
-        return false;
+  //give error message for invalid email, needs DOM element of error and value of email
+  function errorMessageEmail(email, error) {
+    if (email === "") {
+      error.textContent = Content.errorMessage('req');
+      displayErrorMessage(error);
+      return true;
+    } else if (!validateEmail(email)) {
+      error.textContent = Content.errorMessage('valid', Content.localize().textEmailAddress);
+      displayErrorMessage(error);
+      return true;
     }
+    hideErrorMessage(error);
+    return false;
+  }
 
-    //give error message for invalid email, needs DOM element of error and value of email
-    function errorMessageEmail(email, error) {
-        if (email === "") {
-            error.textContent = Content.errorMessage('req');
-            displayErrorMessage(error);
-            return true;
-
-        } else if (!validateEmail(email)) {
-            error.textContent = Content.errorMessage('valid', Content.localize().textEmailAddress);
-            displayErrorMessage(error);
-            return true;
-
-        }
-        hideErrorMessage(error);
-        return false;
+  //give error message for invalid password, needs value of password, repeat of password, and DOM element of error
+  function errorMessagePassword(password, rPassword, error, rError) {
+    hideErrorMessage(error);
+    hideErrorMessage(rError);
+    var errorCounter = 0;
+    if (!/^.+$/.test(password)) {
+      handleError(error, Content.errorMessage('req'));
+      if (!/^.+$/.test(rPassword)) {
+        handleError(rError, Content.errorMessage('req'));
+      }
+      errorCounter++;
     }
-
-    //give error message for invalid password, needs value of password, repeat of password, and DOM element of error
-    function errorMessagePassword(password, rPassword, error, rError) {
-        if (!/^.+$/.test(password)) {
-            hideErrorMessage(rError);
-            error.textContent = Content.errorMessage('req');
-            displayErrorMessage(error);
-
-        	if (!/^.+$/.test(rPassword)) {
-	            rError.textContent = Content.errorMessage('req');
-	            displayErrorMessage(rError);
-
-	            return false;
-
-	        }
-            return false;
-
-        } else if (password !== rPassword) {
-            hideErrorMessage(error);
-            rError.textContent = Content.localize().textPasswordsNotMatching;
-            displayErrorMessage(rError);
-
-            return false;
-
-        } else if (!/^[ -~]+$/.test(password)) {
-            hideErrorMessage(rError);
-            error.textContent = Content.errorMessage('valid', Content.localize().textPassword);
-            displayErrorMessage(error);
-
-            return false;
-
-        } else if (password.length < 6 || password.length > 25) {
-            hideErrorMessage(rError);
-            error.textContent = Content.errorMessage('range', '6-25 ' + Content.localize().textPassword);
-            displayErrorMessage(error);
-
-            return false;
-
-        }
-		hideErrorMessage(error);
-        hideErrorMessage(rError);
-        return true;
-
+    if (password !== rPassword) {
+      handleError(rError, Content.localize().textPasswordsNotMatching);
+      errorCounter++;
     }
+    if (!/^[ -~]+$/.test(password)) {
+      handleError(error, Content.errorMessage('valid', Content.localize().textPassword));
+      errorCounter++;
+    }
+    if (errorCounter === 0){
+      return true;
+    }
+    return false;
+  }
 
-	return {
-		displayErrorMessage: displayErrorMessage,
-        hideErrorMessage: hideErrorMessage,
-        errorMessageEmail: errorMessageEmail,
-        errorMessagePassword: errorMessagePassword
-    };
+  return {
+    displayErrorMessage: displayErrorMessage,
+    hideErrorMessage: hideErrorMessage,
+    errorMessageEmail: errorMessageEmail,
+    errorMessagePassword: errorMessagePassword
+  };
 }());
--- src/javascript/binary/static/virtual_acc_opening.js
+++ src/javascript/binary/static/virtual_acc_opening.js
@@ -1,63 +1,64 @@
 pjax_config_page("virtualws", function(){
-	
-	return {
-		onLoad: function() {
-        	get_residence_list();
-        	Content.populate();
+  return {
+    onLoad: function() {
+      get_residence_list();
+      Content.populate();
+      VirtualAccOpeningUI.setLabel();
+      var form = document.getElementById('virtual-form');
+      var errorEmail = document.getElementById('error-email'),
+          errorPassword = document.getElementById('error-password'),
+          tooltipPassword = document.getElementById('tooltip-password');
 
-			var form = document.getElementById('virtual-form');
-			var errorEmail = document.getElementById('error-email');
+      $('#password').on('input', function() {
+        $('#password-meter').attr('value', testPassword($('#password').val())[0]);
+      });
 
-		    VirtualAccOpeningUI.setLabel();
+      if (form) {
+        $('#virtual-form').submit( function(evt) {
+          evt.preventDefault();
+          Validate.hideErrorMessage(errorEmail);
 
-			if (form) {
+          var email = document.getElementById('email').value,
+              residence = document.getElementById('residence').value,
+              password = document.getElementById('password').value,
+              rPassword = document.getElementById('r-password').value;
 
-				$('#virtual-form').submit( function(evt) {
-					evt.preventDefault();
-					Validate.hideErrorMessage(errorEmail);
+          if (VirtualAccOpeningUI.checkPassword(password, rPassword)){
+            if (testPassword(password)[0] >= 30) {
+              BinarySocket.init({
+                onmessage: function(msg){
+                  var response = JSON.parse(msg.data);
+                  if (response) {
+                    var type = response.msg_type;
+                    var error = response.error;
 
-					var email = document.getElementById('email').value,
-				    	residence = document.getElementById('residence').value,
-				    	password = document.getElementById('password').value,
-						rPassword = document.getElementById('r-password').value;
-
-					if (VirtualAccOpeningUI.checkPassword(password, rPassword)) {
-						
-						BinarySocket.init({
-					        onmessage: function(msg){
-					            var response = JSON.parse(msg.data);
-
-					            if (response) {
-					                var type = response.msg_type;
-					                var error = response.error;
-
-					                if (type === 'new_account_virtual' && !error){
-
-					                    form.setAttribute('action', '/login');
-										form.setAttribute('method', 'POST');
-
-										$('#virtual-form').unbind('submit');
-										form.submit();
-
-					                } else if (type === 'error' || error){
-					                	if (/email address is already in use/.test(error.message)) {
-				                			errorEmail.textContent = Content.localize().textDuplicatedEmail;
-				                		} else if (/required/.test(error.message)) {
-				                			errorEmail.textContent = Content.localize().textTokenMissing;
-				                		} else { 
-				                			errorEmail.textContent = Content.errorMessage('valid', Content.localize().textEmailAddress);
-				                		}
-				                		Validate.displayErrorMessage(errorEmail);
-					                }
-					            }
-					        }
-					    });
-
-					    VirtualAccOpeningData.getDetails(email, password, residence);
-					}
-					
-				});
-			}
-		}
-	};
+                    if (type === 'new_account_virtual' && !error){
+                      form.setAttribute('action', '/login');
+                      form.setAttribute('method', 'POST');
+                      $('#virtual-form').unbind('submit');
+                      form.submit();
+                    } else if (type === 'error' || error){
+                      if (/email address is already in use/.test(error.message)) {
+                        errorEmail.textContent = Content.localize().textDuplicatedEmail;
+                      } else if (/verification_code/.test(error.message)) {
+                        errorEmail.textContent = Content.localize().textTokenMissing;
+                      } else {
+                        errorEmail.textContent = Content.errorMessage('valid', Content.localize().textEmailAddress);
+                      }
+                      Validate.displayErrorMessage(errorEmail);
+                    }
+                  }
+                }
+              });
+              VirtualAccOpeningData.getDetails(email, password, residence);
+            } else if (testPassword(password)[0] < 30) {
+              tooltipPassword.innerHTML = testPassword(password)[1];
+              tooltipPassword.setAttribute('title', text.localize('Try adding upper and lower letters, special characters, and numbers.'))
+              errorPassword.setAttribute('style', 'display:block');
+            }
+          }
+        });
+      }
+    }
+  };
 });
