bfe6e7b6691ed194626025c337a9f240f42a923a
Raunak Kathuria <raunakkathuria@gmail.com>
2015-12-23 17:43:19 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -58662,6 +58662,19 @@ function getUrlVars() {
     return vars;
 }
 
+function replaceQueryParam(param, newval, search) {
+    var regex = new RegExp("([?;&])" + param + "[^&;]*[;&]?");
+    var query = search.replace(regex, "$1").replace(/&$/, '');
+
+    return (query.length > 2 ? query + "&" : "?") + (newval ? param + "=" + newval : '');
+}
+
+if (page.language() === 'JA' && !$.cookie('MyJACookie')) {
+  var str = window.location.search;
+  str = replaceQueryParam('l', 'EN', str);
+  window.location = window.location.pathname + str;
+}
+
 pjax_config_page('/$|/home', function() {
     return {
         onLoad: function() {
@@ -58793,7 +58806,7 @@ pjax_config_page('user/my_account', function() {
                     $.cookie('loginid', loginid, {domain: document.domain.substring(3), path:'/'});
                     page.header.show_or_hide_login_form();
                 }
-                
+
             }
         }
     };
