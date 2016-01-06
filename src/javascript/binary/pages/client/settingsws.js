6f67d57ac0fa09288198527599525ff23e780fb7
binary-hamedanchi <hamedanchi@gmail.com>
2016-01-06 15:05:29 +0800

    User Settings

---
+++ src/javascript/binary/pages/client/settingsws.js
@@ -0,0 +1,33 @@
+var SettingsWS = (function() {
+    "use strict";
+
+    var init = function() {
+        var classHidden = '.invisible',
+            classReal   = '.real';
+
+        if(page.client.is_real) {
+            $(classReal).removeClass(classHidden);
+        }
+        else {
+            $(classReal).addClass(classHidden);
+        }
+    };
+
+    return {
+        init: init
+    };
+}());
+
+
+pjax_config_page("settingsws", function() {
+    return {
+        onLoad: function() {
+            if (!page.client.is_logged_in) {
+                window.location.href = page.url.url_for('login');
+                return;
+            }
+
+            SettingsWS.init();
+        }
+    };
+});
