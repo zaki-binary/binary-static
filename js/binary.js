57a63c698336edcf6e62241e2c5f44ddb7a848da
Raunak Kathuria <raunakkathuria@gmail.com>
2016-01-07 09:50:01 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -59325,6 +59325,41 @@ pjax_config_page("settings/detailsws", function() {
         }
     };
 });
+;var SettingsWS = (function() {
+    "use strict";
+
+    var init = function() {
+        var classHidden = 'invisible',
+            classReal   = '.real';
+
+        if(page.client.is_real) {
+            $(classReal).removeClass(classHidden);
+        }
+        else {
+            $(classReal).addClass(classHidden);
+        }
+
+        $('#settingsContainer').removeClass(classHidden);
+    };
+
+    return {
+        init: init
+    };
+}());
+
+
+pjax_config_page("settingsws", function() {
+    return {
+        onLoad: function() {
+            if (!page.client.is_logged_in) {
+                window.location.href = page.url.url_for('login');
+                return;
+            }
+
+            SettingsWS.init();
+        }
+    };
+});
 ;var sidebar_scroll = function(elm_selector) {
     elm_selector.on('click', '#sidebar-nav li', function() {
         var clicked_li = $(this);
