78e150fe80a8c842852d52815dab1e956df2cbd7
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-01-13 15:14:30 +0800

    Adding some fix for selfexclusion, my_accounws, vitual_top-up

--- src/javascript/binary/static/cashier/my_accountws.js
+++ src/javascript/binary/static/cashier/my_accountws.js
@@ -1,46 +1,22 @@
 var my_accountws = (function(){
 
     "use strict";
-    var currType;
 
     var init = function(){
     	$("#VRT_topup_link").hide();
-    	BinarySocket.send({"authorize": $.cookie('login'), "req_id": 1 });
-    };
 
-    var isAuthorized = function(response){
-    	var str , bal ;
-    	if(response.echo_req.req_id){
-	    	if("error" in response) {
-	            if("message" in response.error) {
-	                console.log(message);
-	            }
-	            return false;
-	        }
-	    	else{
-	    		currType = response.authorize.currency;
-	    		bal =  response.authorize.balance;
-	    		if(parseInt(response.req_id) === 1 && bal < 1000){
-	    			str = "Deposit "+ currType + " 10000 virtual money into your account ";
-	    			$("#VRT_topup_link").show();
-	    			$("#VRT_topup_link a").text(text.localize(str));
-	    		}
-	    	}
-    	}
+        var currType = TUser.get().currency;
+        var bal =  TUser.get().balance;
 
-    };
-
-    var apiResponse = function(response){
-    	var type = response.msg_type;
-    	if(type === "authorize" || (type === "error" && "authorize" in response.echo_req))
-        {
-            isAuthorized(response);
+        if(bal < 1000 && (/^VRT/.test(TUser.get().loginid) === true ) ){
+            var str = "Deposit "+ currType + " 10000 virtual money into your account ";
+            $("#VRT_topup_link").show();
+            $("#VRT_topup_link a").text(text.localize(str));
         }
     };
 
     return {
-    	init : init,
-    	apiResponse : apiResponse
+    	init : init
 
     };
 
@@ -55,17 +31,13 @@ pjax_config_page("user/my_account", function() {
                 window.location.href = page.url.url_for('login');
                 return;
             }
-        	BinarySocket.init({
-                onmessage: function(msg){
-                    var response = JSON.parse(msg.data);
-                    if (response) {
-                        my_accountws.apiResponse(response);
-                          
-                    }
-                }
-            });	
 
-            my_accountws.init();
+            BinarySocket.init({
+                onauth : function(){
+                    my_accountws.init();
+                }
+
+            });
         }
     };
 });
