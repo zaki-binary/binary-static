f286be0065b3ca3317564f88a1c9931e23cb2b6d
negar <negar@binary.com>
2016-01-13 18:39:24 +0800

    shorten IE function

--- src/javascript/binary/pages/frontend.js
+++ src/javascript/binary/pages/frontend.js
@@ -445,25 +445,10 @@ if (page.language() === 'JA' && !$.cookie('MyJACookie')) {
   window.location = window.location.pathname + str;
 }
 
-function detectIE() {
-  var ua = window.navigator.userAgent;
 
-  var msie = ua.indexOf('MSIE ');
-  if (msie > 0) {
-    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-  }
-
-  var trident = ua.indexOf('Trident/');
-  if (trident > 0) {
-    var rv = ua.indexOf('rv:');
-    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-  }
-
-  var edge = ua.indexOf('Edge/');
-  if (edge > 0) {
-    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-  }
-  return false;
+// returns true if internet explorer browser
+function isIE() {
+  return /(msie|trident|edge)/i.test(window.navigator.userAgent) && !window.opera;
 }
 
 pjax_config_page('/$|/home', function() {
