92b0c5397f6554910bc6c0db193ac8d64bd41b07
negar <negar@binary.com>
2016-01-19 13:08:26 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -68809,12 +68809,16 @@ var ViewBalanceUI = (function(){
 
     function getDetails(email, password, residence){
         var req = {
-                    new_account_virtual: 1, 
-                    email: email, 
-                    client_password: password, 
-                    residence: residence, 
+                    new_account_virtual: 1,
+                    email: email,
+                    client_password: password,
+                    residence: residence,
                     verification_code: $.cookie('verify_token')
                 };
+
+        if ($.cookie('affiliate_token')) {
+            req.affiliate_token = $.cookie('affiliate_token');
+        }
 
         BinarySocket.send(req);
     }
