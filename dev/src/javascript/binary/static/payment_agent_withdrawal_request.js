9867a3dd2f364ac3966acea415e6b1deca7d6c75
ashkanx <ashkan.nemati@gmail.com>
2016-04-08 14:54:29 +0800

    Auto-generated commit

--- dev/src/javascript/binary/static/payment_agent_withdrawal_request.js
+++
@@ -1,9 +0,0 @@
-pjax_config_page("paymentagent/request_withdrawws", function(){
-
-  return {
-    onLoad: function() {
-      $('#client_email').html(page.user.email);
-      BinarySocket.send({verify_email:page.user.email, type:'paymentagent_withdraw'});
-    }
-  };
-});
