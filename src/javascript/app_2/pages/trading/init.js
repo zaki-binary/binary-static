0d18a2bf217a5d1df71e7050d44431aa3a741aa8
kelly <kelly@binary.com>
2017-12-06 11:16:56 +0800

    Add Material IO components

--- src/javascript/app_2/pages/trading/init.js
+++ src/javascript/app_2/pages/trading/init.js
@@ -1,8 +1,12 @@
 const State = require('../../../_common/storage').State;
+const mdcDrawer = require('@material/drawer');
 
 const Trading = (() => {
     const onLoad = () => {
         State.set('is_trading_2', true);
+        const MDCTemporaryDrawer = mdcDrawer.MDCTemporaryDrawer;
+        const drawer = new MDCTemporaryDrawer(document.querySelector('.mdc-temporary-drawer'));
+        document.querySelector('.menu-toggle').addEventListener('click', () => drawer.open = true);
     };
 
     const onUnload = () => {
