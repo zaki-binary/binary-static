0d404bb8c0639136d49e0e30bbd7c6da99383aab
zakibinary <zaki@Zakis-MacBook-Pro-2.local>
2017-12-07 17:58:11 +0800

    updated header and pop-menu

--- src/javascript/app_2/pages/trading/init.js
+++ src/javascript/app_2/pages/trading/init.js
@@ -1,17 +1,13 @@
-const State       = require('../../../_common/storage').State;
-const mdcDrawer   = require('@material/drawer');
-const mdcDialog   = require('@material/dialog');
-// const mdcToolbar = require('@material/toolbar');
-// const mdcTabs = require('@material/tabs');
-const mdcTextField = require('@material/textfield');
+const State        = require('../../../_common/storage').State;
+const mdcDrawer    = require('@material/drawer');
+const mdcDialog    = require('@material/dialog');
+// const mdcTextField = require('@material/textfield');
 // const mdcSelect = require('@material/select');
 // const mdcRadio = require('@material/radio');
 
 const Trading = (() => {
     const onLoad = () => {
-        console.log('hello_word');
         State.set('is_trading_2', true);
-
         const MDCTemporaryDrawer = mdcDrawer.MDCTemporaryDrawer;
         const drawer = new MDCTemporaryDrawer(document.querySelector('.mdc-temporary-drawer'));
         const notify_drawer = new MDCTemporaryDrawer(document.querySelector('.notifications-list'));
@@ -19,23 +15,31 @@ const Trading = (() => {
             evt.preventDefault();
             drawer.open = true;
         });
+        document.querySelector('.menu-close').addEventListener('click', (evt) => {
+            evt.preventDefault();
+            if(drawer.open) drawer.open = false;
+        });
         document.querySelector('.notifications-toggle').addEventListener('click', (evt) => {
             evt.preventDefault();
             notify_drawer.open = true;
         });
-        const amountField = new mdcTextField.MDCTextField(document.querySelector('.amount'));
-        const textField = new mdcTextField.MDCTextField(document.querySelector('.text'));
+        document.querySelector('.notifications-close').addEventListener('click', (evt) => {
+            evt.preventDefault();
+            if(notify_drawer.open) notify_drawer.open = false;
+        });
+        // const amountField = new mdcTextField.MDCTextField(document.querySelector('.amount'));
+        // const textField = new mdcTextField.MDCTextField(document.querySelector('.text'));
         const dialog = new mdcDialog.MDCDialog(document.querySelector('#mdc-dialog-with-list'));
 
-        console.log(amountField);
-        console.log(textField);
+        // console.log(amountField);
+        // console.log(textField);
 
         dialog.listen('MDCDialog:accept', () => {
-            console.log('confirm');
+            // console.log('confirm');
         });
 
         dialog.listen('MDCDialog:cancel', () => {
-            console.log('discard');
+            // console.log('discard');
         });
 
         document.querySelector('#modal-popup').addEventListener('click', (evt) => {
